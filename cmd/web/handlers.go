ea0b46f30ad99a700d2eb7b38cbb195d62d489f4
Dieveral <dieveral@gmail.com>
2022-12-06 17:49:09 +0300

    lesson 6

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,21 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
-
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	log.Println("Server started on http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
-}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
